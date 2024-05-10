4e35707246bd67899e2abe9243048d0f9ea54610
yunusemrealtiner1 <yunusemre12500@gmail.com>
2024-05-10 19:26:51 +0300

    Rete limit implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::sync::Mutex;
+use std::{sync::Mutex, time};
 
+use actix_limitation::{Limiter, RateLimiter};
 use actix_web::{web, App, HttpServer};
 use repositories::v1::TodoRepository;
 use services::v1::TodoService;
@@ -16,11 +17,24 @@ async fn main() -> std::io::Result<()> {
 
     let todo_repository = TodoRepository::new(database);
 
-    let todo_service = TodoService::new(todo_repository);
+    let todo_service = web::Data::new(Mutex::new(TodoService::new(todo_repository)));
+
+    let redis_url = std::env::var("REDIS_URL").unwrap_or("redis://127.0.0.1".to_string());
+
+    let limiter = web::Data::new(
+        Limiter::builder(redis_url)
+            .limit(120)
+            .period(time::Duration::from_secs(60))
+            .key_by(|request| Some(request.connection_info().peer_addr().unwrap().to_string()))
+            .build()
+            .expect("Build limiter failed."),
+    );
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(Mutex::new(todo_service.clone())))
+            .wrap(RateLimiter::default())
+            .app_data(limiter.clone())
+            .app_data(todo_service.clone())
             .service(
                 web::scope("/v1")
                     .service(controllers::v1::todos::create_todo)
